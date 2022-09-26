2f01a719de26316fbdb393460b52ac3d5aea29de
Elizabeth-Foluso <elizabetholojede@rocketmail.com>
2022-09-26 11:47:04 -0700

    main

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,21 @@
 #include "main.h"
-/**
- * _strcat - concats two arrays
- *
- * @dest: destination of concat
- * @src: source array to concat
- *
- * Return: char value
- */
-char *_strcat(char *dest, char *src)
+#include <string.h>
+/*define data type*/
+
+char *_strcat(char *dest, char *src);
 {
-	int i;
-	int j;
-
-	for (j = 0; dest[j] != '\0'; j++)
-	{}
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[j + i] = src[i];
-	}
-	dest[j + i] = '\0';
-
-	return (dest);
-
+int count = 0, count2 = 0;
+while (*(dest + count) != '\0')
+{
+count++;
+}
+while (count2 >= 0)
+{
+*(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
+break;
+count++;
+count2++;
+}
+return (dest);
 }
