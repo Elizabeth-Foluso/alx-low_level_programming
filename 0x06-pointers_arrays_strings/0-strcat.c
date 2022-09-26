a89581b3e12cc706aec99cf146f4e027e7c0b236
Elizabeth-Foluso <elizabetholojede@rocketmail.com>
2022-09-26 12:27:28 -0700

    main

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,18 +4,15 @@
 
 char *_strcat(char *dest, char *src);
 {
-int count = 0, count2 = 0;
-while (*(dest + count) != '\0')
-{
-count++;
-}
-while (count2 >= 0)
-{
-*(dest + count) = *(src + count2);
-if (*(src + count2) == '\0')
-break;
-count++;
-count2++;
-}
-return (dest);
+	char s1[] = "Hello ";
+	char s2[] = "World!\n";
+	char *ss;
+
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ss = _strcat(s1, s2);
+	printf("%s\n", s1);
+	printf("%s", s2);
+	printf("%s", ss);
+	return (dest);
 }
